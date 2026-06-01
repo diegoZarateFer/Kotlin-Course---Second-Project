94ee4570db35efa0dfb23f14506a6b12dcd8b80c
diegoZarate <diego.zarate@memphis.mx>
2026-06-01 11:33:39 -0600

    Functional Programming.

--- src/main/kotlin/profile/Gender.kt
+++ src/main/kotlin/profile/Gender.kt
@@ -4,7 +4,7 @@ import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
-enum class Gender {
+enum class  Gender {
     @SerialName("Male")
     MALE,
 
--- src/main/kotlin/test/Test.kt
+++ src/main/kotlin/test/Test.kt
@@ -1,63 +1,25 @@
 package test
 
-import kotlinx.serialization.json.Json
 import java.io.File
 
+val numbers = mutableListOf<Int>()
+
 fun main() {
-
-    val booksFile = File("books.json")
-    val books = retrieveBooks(file = booksFile)
-    for(book in books) {
-        println(book)
-    }
-
-//    val books = mutableListOf<Book>(
-//        Book(title = "Book 1", year = 1900, author = "Jean Jakes"),
-//        Book(title = "Book 2", year = 2020, author = "John Smith"),
-//    )
-
-//    saveBooks(books = books, file = booksFile)
-
-
-
-//    val file = File("items.json")
-//    writeToFile(file)
-
-//    val items = readFromFile(file)
-//    for(item in items) {
-//        println(item)
-//    }
+    println(sum(8,7))
 }
 
-fun readFromFile(file: File): List<Item> {
-    val content = file.readText().trim()
-    return Json.decodeFromString<List<Item>>(content)
+//fun sum(a: Int): Int {
+//    val file = File("tests.txt")
+//    val num = file.readText().trim().toInt()
+//    return a + num
+//}
+
+fun sum(a: Int, b: Int): Int {
+    val result = a + b
+    numbers.add(result)
+    return result
 }
 
-fun writeToFile(file: File) {
-
-    val items = mutableListOf<Item>()
-    while (true) {
-        print("Enter id or 0 to exit: ")
-        val id = readln().toInt()
-        if (id == 0) break
-
-        print("Enter name: ")
-        val name = readln()
-
-        val item = Item(id, name)
-        items.add(item)
-    }
-
-    val serializedItems = Json.encodeToString(items)
-    file.writeText(serializedItems)
-}
-
-fun saveBooks(books: List<Book>, file:File) {
-    file.writeText(Json.encodeToString(books))
-}
-
-fun retrieveBooks(file: File): List<Book> {
-    val serializedBooks = file.readText().trim()
-    return Json.decodeFromString(serializedBooks)
-}
+//fun sum(a: Int, b: Int): Int {
+//    return a + b;
+//}
