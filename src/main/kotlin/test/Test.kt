1f88a6f224623b0cbb75b1df546c01b9e1c4fd4f
diegoZarate <diego.zarate@memphis.mx>
2026-06-05 11:25:38 -0600

    The With and Apply Functions.

--- src/main/kotlin/test/Test.kt
+++ src/main/kotlin/test/Test.kt
@@ -3,4 +3,39 @@ package test
 var age: Int? = 20
 
 fun main() {
+    exampleWith()
+}
+
+inline fun <T> T.myApply(block: T.() -> Unit): T {
+    block()
+    return this
+}
+
+inline fun <T, R> myWith(myObject: T, block: T.() -> R): R {
+    return myObject.block()
+}
+
+fun exampleApply() {
+    mutableListOf<Int>().apply {
+        while (true) {
+            print("Enter number or 0 to exit: ")
+            val num = readln().toInt().takeIf { it != 0 } ?: break
+            add(num)
+        }
+    }.forEach {
+        println(it)
+    }
+}
+
+fun exampleWith() {
+    myWith(mutableListOf<Int>()) {
+        while (true) {
+            print("Enter number or 0 to exit: ")
+            val num = readln().toInt().takeIf { it != 0 } ?: break
+            add(num)
+        }
+
+        println("MAX: ${max()}")
+        println("MIN: ${min()}")
+    }
 }
